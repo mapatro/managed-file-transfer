def9c113b60f356c4d416565878e6f842528be60
mpatro <mpatro@gmail.com>
2018-06-11 23:20:25 -0400

    initial version

---
+++ managed-file-transfer/src/main/java/com/cube/mft/ManagedFileTransferApplication.java
@@ -0,0 +1,12 @@
+package com.cube.mft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagedFileTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagedFileTransferApplication.class, args);
+	}
+}
---
+++ managed-file-transfer/src/test/java/com/cube/mft/ManagedFileTransferApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cube.mft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagedFileTransferApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
